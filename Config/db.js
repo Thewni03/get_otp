603f26bce5a1b8e3f509a1c937ee45f06d082efc
Thewni Mahathanthri <144580755+Thewni03@users.noreply.github.com>
2026-02-20 11:24:59 +0530

    security issues

--- Config/db.js
+++ Config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const User = require('../models/user');
 
-const MONGO_URL = "mongodb+srv://thewni2003_db_user:87tvn6usDM8gsSfV@cluster0.nvzk1r0.mongodb.net/otpDB?retryWrites=true&w=majority";
+const MONGO_URL = "ur url";
 
 const connectDB = async () => {
   try {
